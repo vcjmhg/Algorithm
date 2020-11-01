e8fe2cf21874c406fb88469cf460b9a120b6f7f1
vcjmhg <zhaojianheng618@163.com>
2020-11-01 18:28:40 +0800

    通过状态压缩，对原来的方法进行优化

--- src/com/shuzijun/leetcode/editor/cn/PartitionEqualSubsetSum.java
+++ src/com/shuzijun/leetcode/editor/cn/PartitionEqualSubsetSum.java
@@ -44,29 +44,26 @@ public class PartitionEqualSubsetSum {
   // leetcode submit region begin(Prohibit modification and deletion)
   class Solution {
     public boolean canPartition(int[] nums) {
-      if (nums.length == 0) {
-        return true;
+      int sum = 0, n = nums.length;
+      for (int num : nums) {
+        sum += num;
       }
-      int N = nums.length;
-      int sum = 0;
-      for (int i = 0; i < N; i++) {
-        sum += nums[i];
-      }
-      if ((sum & 1) == 1) {
+      if (sum % 2 != 0) {
         return false;
       }
-      int halfSum = sum / 2;
-      int[][] dp = new int[N + 1][halfSum + 1];
-      for (int i = 1; i <= N; i++) {
-        for (int w = 1; w <= halfSum; w++) {
-          if (w < nums[i - 1]) {
-            dp[i][w] = dp[i - 1][w];
-          } else {
-            dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - nums[i - 1]] + nums[i - 1]);
+      sum = sum / 2;
+      boolean [] dp=new boolean[sum + 1] ;
+      // base case
+      dp[0] = true;
+
+      for (int i = 0; i < n; i++)
+        for (int j = sum; j >= 0; j--) {
+          if (j - nums[i] >= 0) {
+            dp[j] = dp[j] || dp[j - nums[i]];
           }
         }
-      }
-      return dp[N][halfSum] == halfSum;
+
+      return dp[sum];
     }
   }
   // leetcode submit region end(Prohibit modification and deletion)
--- src/dp/LeetCode416.java
+++ src/dp/LeetCode416.java
@@ -34,4 +34,32 @@ public class LeetCode416 {
     }
     return dp[N][halfSum] == halfSum;
   }
+
+  /**
+   * 对状态进行压缩，因为递推的过程中只用到了两个状态，因此可以通过状态压缩进行优化
+   * @param nums
+   * @return
+   */
+  public boolean canPartition2(int[] nums) {
+    int sum = 0, n = nums.length;
+    for (int num : nums) {
+      sum += num;
+    }
+    if (sum % 2 != 0) {
+      return false;
+    }
+    sum = sum / 2;
+    boolean [] dp=new boolean[sum + 1] ;
+    // base case
+    dp[0] = true;
+
+    for (int i = 0; i < n; i++)
+      for (int j = sum; j >= 0; j--) {
+        if (j - nums[i] >= 0) {
+          dp[j] = dp[j] || dp[j - nums[i]];
+        }
+      }
+
+    return dp[sum];
+  }
 }
