03e7513bb80c62dab8028f96e2a668af243ca671
vcjmhg <zhaojianheng618@163.com>
2020-11-15 19:30:10 +0800

    解决使用使用模板不通过问题：原因Integer类型会缓存256个值，如果使用等号比较会出现问题

--- src/com/shuzijun/leetcode/editor/cn/PermutationInString.java
+++ src/com/shuzijun/leetcode/editor/cn/PermutationInString.java
@@ -50,36 +50,33 @@ public class PermutationInString {
       }
       Map<Character, Integer> need = new HashMap<>();
       Map<Character, Integer> window = new HashMap<>();
-      // to char array
-      char[] t_array = t.toCharArray();
-      char[] s_array = s.toCharArray();
       // init need
-      for (char c : t_array) {
+      for (char c : t.toCharArray()) {
         need.put(c, need.getOrDefault(c, 0) + 1);
       }
       // define variable
       int left = 0, right = 0;
       int valid = 0;
-      while (right < s_array.length) {
-        char c = s_array[right];
+      while (right < s.length()) {
+        char c = s.charAt(right);
         right++;
         // update right window
         if (need.containsKey(c)) {
           window.put(c, window.getOrDefault(c, 0) + 1);
-          if (need.get(c) == window.get(c)) {
+          if (need.get(c).equals(window.get(c))) {
             valid++;
           }
         }
         // shrink left window
-        //每一次窗口的尺寸比need的尺寸大的时候都会进行瘦身操作,一直移动到比need的尺寸小1结束
-        while (right - left >= t_array.length) {
+        // 每一次窗口的尺寸比need的尺寸大的时候都会进行瘦身操作,一直移动到比need的尺寸小1结束
+        while (right - left >= t.length()) {
           if (valid == need.size()) {
             return true;
           }
-          char d = s_array[left];
+          char d = s.charAt(left);
           left++;
           if (need.containsKey(d)) {
-            if (window.get(d) == need.get(d)) {
+            if (window.get(d).equals(need.get(d))) {
               valid--;
             }
             window.put(d, window.get(d) - 1);
